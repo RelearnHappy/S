3246b15d1f55605c2b11f40af7a426b955fd8e77
RelearnHappy <165519652+RelearnHappy@users.noreply.github.com>
2025-03-17 01:23:41 +0700

    Update surahs.js

--- data/surahs.js
+++ data/surahs.js
@@ -9,31 +9,31 @@ export const surahs = [
     },
     {
       id: 2,
-      name: "Al-Baqara",
-      englishName: "The Cow",
-      numberOfAyahs: 286,
-      youtubeId: "OL01mQU0UJQ" // Example YouTube ID for Al-Baqara recitation
+      name: "Al-Alaq",
+      englishName: "The Clot",
+      numberOfAyahs: 19,
+      youtubeId: "u7pnPt6mpUU" // Example YouTube ID for Al-Baqara recitation
     },
     {
       id: 3,
-      name: "Ali 'Imran",
-      englishName: "Family of Imran",
-      numberOfAyahs: 200,
-      youtubeId: "1xS0PCX4Xuw" // Example YouTube ID
+      name: "Al Mulk",
+      englishName: "The Sovereignty",
+      numberOfAyahs: 30,
+      youtubeId: "WAAyC2ihdBk" // Example YouTube ID
     },
     {
       id: 4,
-      name: "An-Nisa",
-      englishName: "The Women",
-      numberOfAyahs: 176,
-      youtubeId: "0m02PtLQxGM" // Example YouTube ID
+      name: "JUZ 20",
+      englishName: "-",
+      numberOfAyahs: -,
+      youtubeId: "DUsmoihR90o" // Example YouTube ID
     },
     {
       id: 5,
-      name: "Al-Ma'ida",
-      englishName: "The Table Spread",
-      numberOfAyahs: 120,
-      youtubeId: "X35Wxd-GjZU" // Example YouTube ID
+      name: "jUZ 30",
+      englishName: "-",
+      numberOfAyahs: -,
+      youtubeId: "4EOokEXW604" // Example YouTube ID
     }
     // Add more surahs as needed
   ];
