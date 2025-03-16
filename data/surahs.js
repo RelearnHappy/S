484896d89995074134511900f1691463c66acc05
RelearnHappy <165519652+RelearnHappy@users.noreply.github.com>
2025-03-17 02:13:16 +0700

    commit 173 lagi

--- data/surahs.js
+++ data/surahs.js
@@ -1,40 +1,39 @@
 // data/surahs.js
 export const surahs = [
-    {
-      id: 1,
-      name: "Al-Fatihah",
-      englishName: "The Opening",
-      numberOfAyahs: 7,
-      youtubeId: "qCQOhiDAanQ" // Example YouTube ID for Al-Fatiha recitation
-    },
-    {
-      id: 2,
-      name: "Al-Alaq",
-      englishName: "The Clot",
-      numberOfAyahs: 19,
-      youtubeId: "u7pnPt6mpUU" // Example YouTube ID for Al-Baqara recitation
-    },
-    {
-      id: 3,
-      name: "Al Mulk",
-      englishName: "The Sovereignty",
-      numberOfAyahs: 30,
-      youtubeId: "WAAyC2ihdBk" // Example YouTube ID
-    },
-    {
-      id: 4,
-      name: "Juz 29",
-      englishName: "Juz 29",
-      numberOfAyahs: 000,
-      youtubeId: "DUsmoihR90o" // Example YouTube ID
-    },
-    {
-      id: 5,
-      name: "Juz 30",
-      englishName: "Juz 30",
-      numberOfAyahs: 000,
-      youtubeId: "4EOokEXW604" // Example YouTube ID
-    }
-    // Add more surahs as needed
-  ];
-  
+  {
+    id: 1,
+    name: "Al-Fatihah",
+    englishName: "The Opening",
+    numberOfAyahs: 7,
+    youtubeId: "qCQOhiDAanQ" // Example YouTube ID for Al-Fatiha recitation
+  },
+  {
+    id: 2,
+    name: "Al-Alaq",
+    englishName: "The Clot",
+    numberOfAyahs: 19,
+    youtubeId: "u7pnPt6mpUU" // Example YouTube ID for Al-Baqara recitation
+  },
+  {
+    id: 3,
+    name: "Al Mulk",
+    englishName: "The Sovereignty",
+    numberOfAyahs: 30,
+    youtubeId: "WAAyC2ihdBk" // Example YouTube ID
+  },
+  {
+    id: 4,
+    name: "Juz 29",
+    englishName: "Juz 29",
+    numberOfAyahs: 431,
+    youtubeId: "DUsmoihR90o" // Example YouTube ID
+  },
+  {
+    id: 5,
+    name: "Juz 30",
+    englishName: "Juz 30",
+    numberOfAyahs: 564,
+    youtubeId: "4EOokEXW604" // Example YouTube ID
+  }
+  // Add more surahs as needed
+];
