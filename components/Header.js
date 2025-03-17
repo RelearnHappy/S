5a6d536dea289597d35eef726d1654761f2f7034
RelearnHappy <165519652+RelearnHappy@users.noreply.github.com>
2025-03-17 22:13:45 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,21 @@
-// components/Header.js
+// 4. Modifikasi: components/Header.js
 import Link from 'next/link';
+import ThemeToggle from './ThemeToggle';
 import styles from '../styles/Home.module.css';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/">
-        <div className={styles.logo}>
-          <img src="/logo.png" alt="Qur'an Recitation App" width={50} height={50} />
-          <h1>Qur'an Recitations</h1>
-        </div>
-      </Link>
+      <div className={styles.headerContent}>
+        <Link href="/">
+          <div className={styles.logo}>
+            <img src="/logo.png" alt="Qur'an Recitation App" width={50} height={50} />
+            <h1>Qur'an Recitations</h1>
+          </div>
+        </Link>
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
+
