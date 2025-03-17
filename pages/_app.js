851314d8204865c3bc697d5db6a270afa1bedbed
RelearnHappy <165519652+RelearnHappy@users.noreply.github.com>
2025-03-17 22:10:42 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
-// pages/_app.js
+// 2. Modifikasi: pages/_app.js
 import '../styles/globals.css';
+import { ThemeProvider } from '../context/ThemeContext';
+import { AnimatePresence } from 'framer-motion'; // Tambahkan framer-motion ke package.json
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <ThemeProvider>
+      <AnimatePresence mode="wait" initial={false}>
+        <Component {...pageProps} key={router.asPath} />
+      </AnimatePresence>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
