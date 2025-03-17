6eb14ee2e93259e29fbfd7b0bf184c768cabc537
RelearnHappy <165519652+RelearnHappy@users.noreply.github.com>
2025-03-17 21:28:44 +0700

    commit 173

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 // pages/_app.js
+import { useState } from 'react';
 import '../styles/globals.css';
+import Header from '../components/Header'; // Pastikan path ke Header benar
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = useState('dark'); // State awal: dark mode
+
+  return (
+    <div className={theme}> {/* Class tema diterapkan di root */}
+      <Header theme={theme} setTheme={setTheme} />
+      <Component {...pageProps} /> {/* Render halaman aktif */}
+    </div>
+  );
 }
 
 export default MyApp;
