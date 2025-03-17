bb47579af44cc52cb8ab88dce2af451eb8be6f58
RelearnHappy <165519652+RelearnHappy@users.noreply.github.com>
2025-03-17 22:38:12 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-// 2. Modifikasi: pages/_app.js
 import '../styles/globals.css';
 import { ThemeProvider } from '../context/ThemeContext';
 import { AnimatePresence } from 'framer-motion'; // Tambahkan framer-motion ke package.json
