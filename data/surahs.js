33ed5fad06d084b92d549255dd387eb2b8d5e7d3
RelearnHappy <165519652+RelearnHappy@users.noreply.github.com>
2025-03-19 01:24:39 +0700

    Update surahs.js

--- data/surahs.js
+++ data/surahs.js
@@ -1,39 +1,261 @@
 // data/surahs.js
 export const surahs = [
   {
-    id: 1,
-    name: "Al-Fatihah",
-    englishName: "The Opening",
-    numberOfAyahs: 7,
-    youtubeId: "qCQOhiDAanQ" // Example YouTube ID for Al-Fatiha recitation
+    id: 78,
+    name: "An-Naba",
+    englishName: "The Tidings",
+    numberOfAyahs: 40,
+    youtubeId: "TKDM6cPhWkc" // Example YouTube ID
   },
   {
-    id: 2,
-    name: "Al-Alaq",
-    englishName: "The Clot",
+    id: 79,
+    name: "An-Nazi'at",
+    englishName: "Those who drag forth",
+    numberOfAyahs: 46,
+    youtubeId: "OU5anLNwi6c" // Example YouTube ID
+  },
+  {
+    id: 80,
+    name: "'Abasa",
+    englishName: "He Frowned",
+    numberOfAyahs: 42,
+    youtubeId: "A83BpnFYIj0" // Example YouTube ID
+  },
+  {
+    id: 81,
+    name: "At-Takwir",
+    englishName: "The Overthrowing",
+    numberOfAyahs: 29,
+    youtubeId: "V1F6OJhwbpo" // Example YouTube ID
+  },{
+    id: 82,
+    name: "Al-Infitar",
+    englishName: "The Cleaving",
     numberOfAyahs: 19,
-    youtubeId: "u7pnPt6mpUU" // Example YouTube ID for Al-Baqara recitation
+    youtubeId: "0nrKOmNXjSw" // Example YouTube ID
   },
   {
-    id: 3,
-    name: "Al Mulk",
-    englishName: "The Sovereignty",
+    id: 83,
+    name: "Al-Mutaffifin",
+    englishName: "The Defrauding",
+    numberOfAyahs: 36,
+    youtubeId: "MaF75o_kZqc" // Example YouTube ID
+  },
+  {
+    id: 84,
+    name: "Al-Inshiqaq",
+    englishName: "The Sundering",
+    numberOfAyahs: 25,
+    youtubeId: "622cgJAxtXs" // Example YouTube ID
+  },
+  {
+    id: 85,
+    name: "Al-Buruj",
+    englishName: "The Mansions of the Stars",
+    numberOfAyahs: 22,
+    youtubeId: "acrZnvFMs1Y" // Example YouTube ID
+  },
+  {
+    id: 86,
+    name: "At-Tariq",
+    englishName: "The Nightcommer",
+    numberOfAyahs: 17,
+    youtubeId: "znRkGKNWo1Q" // Example YouTube ID
+  },
+  {
+    id: 87,
+    name: "Al-A'la",
+    englishName: "The Most High",
+    numberOfAyahs: 19,
+    youtubeId: "kkq4b1KL8ZM" // Example YouTube ID
+  },
+  {
+    id: 88,
+    name: "Al-Ghashiyah",
+    englishName: "The Overwhelming",
+    numberOfAyahs: 26,
+    youtubeId: "tDR7WyjrQ2Q" // Example YouTube ID
+  },
+  {
+    id: 89,
+    name: "Al-Fajr",
+    englishName: "The Dawn",
     numberOfAyahs: 30,
-    youtubeId: "WAAyC2ihdBk" // Example YouTube ID
+    youtubeId: "a73vAxfR5DQ" // Example YouTube ID
   },
   {
-    id: 4,
-    name: "Juz 29",
-    englishName: "Juz 29",
-    numberOfAyahs: 431,
-    youtubeId: "DUsmoihR90o" // Example YouTube ID
+    id: 90,
+    name: "Al-Balad",
+    englishName: "The City",
+    numberOfAyahs: 20,
+    youtubeId: "OU6ReuAtALk" // Example YouTube ID
   },
   {
-    id: 5,
-    name: "Juz 30",
-    englishName: "Juz 30",
-    numberOfAyahs: 564,
-    youtubeId: "4EOokEXW604" // Example YouTube ID
-  }
-  // Add more surahs as needed
+    id: 91,
+    name: "Ash-Shams",
+    englishName: "The Sun",
+    numberOfAyahs: 15,
+    youtubeId: "vHwtEcPm6nw" // Example YouTube ID
+  },
+  {
+    id: 92,
+    name: "Al-Layl",
+    englishName: "The Night",
+    numberOfAyahs: 21,
+    youtubeId: "-kGShwlCD0s" // Example YouTube ID
+  },
+  {
+    id: 93,
+    name: "Ad-Duhaa",
+    englishName: "The Morning Hours",
+    numberOfAyahs: 11,
+    youtubeId: "SktKRyYjumo " // Example YouTube ID
+  },
+  {
+    id: 94,
+    name: "Ash-Sharh",
+    englishName: "The Relief",
+    numberOfAyahs: 8,
+    youtubeId: "Bt0Kw9iG_jc" // Example YouTube ID
+  },
+  {
+    id: 95,
+    name: "At-Tin",
+    englishName: "The Fig",
+    numberOfAyahs: 8,
+    youtubeId: "tSFVaSLCpyc" // Example YouTube ID
+  },
+  {
+    id: 96,
+    name: "Al-'Alaq",
+    englishName: "The Iron",
+    numberOfAyahs: 29,
+    youtubeId: "u7pnPt6mpUU" // Example YouTube ID
+  },
+  {
+    id: 97,
+    name: "Al-Qadr",
+    englishName: "The Power",
+    numberOfAyahs: 5,
+    youtubeId: "5sMLz3QaD54" // Example YouTube ID
+  },
+  {
+    id: 98,
+    name: "Al-Bayyinah",
+    englishName: "The Clear Proof",
+    numberOfAyahs: 8,
+    youtubeId: "gsESaXr6UiA" // Example YouTube ID
+  },
+  {
+    id: 99,
+    name: "Az-Zalzalah",
+    englishName: "The Earthquake",
+    numberOfAyahs: 8,
+    youtubeId: "AVlDAyTBxic" // Example YouTube ID
+  },
+  {
+    id: 100,
+    name: "Al-'Adiyat",
+    englishName: "The Courser",
+    numberOfAyahs: 11,
+    youtubeId: "UruG8DNFIoo" // Example YouTube ID
+  },
+  {
+    id: 101,
+    name: "Al-Qari'ah",
+    englishName: "The Calamity",
+    numberOfAyahs: 11,
+    youtubeId: "1_DlhZX5oNg" // Example YouTube ID
+  },
+  {
+    id: 102,
+    name: "At-Takathur",
+    englishName: "The Rivalry in world increase",
+    numberOfAyahs: 8,
+    youtubeId: "y5m7JlB_7yQ" // Example YouTube ID
+  },
+  {
+    id: 103,
+    name: "Al-'Asr",
+    englishName: "The Declining Day",
+    numberOfAyahs: 3,
+    youtubeId: "diu9g1DHFVQ" // Example YouTube ID
+  },
+  {
+    id: 104,
+    name: "Al-Humazah",
+    englishName: "The Traducer",
+    numberOfAyahs: 9,
+    youtubeId: "AmTQRUKub0k" // Example YouTube ID
+  },
+  {
+    id: 105,
+    name: "Al-Fil",
+    englishName: "The Elephant",
+    numberOfAyahs: 5,
+    youtubeId: "pvYRdATOHa8" // Example YouTube ID
+  },
+  {
+    id: 106,
+    name: "Quraysh",
+    englishName: "Quraysh",
+    numberOfAyahs: 4,
+    youtubeId: "Z3-qfPWXym0" // Example YouTube ID
+  },
+  {
+    id: 107,
+    name: "Al-Ma'un",
+    englishName: "The Small kindnesses",
+    numberOfAyahs: 7,
+    youtubeId: "75l_g0dQiyk" // Example YouTube ID
+  },
+  {
+    id: 108,
+    name: "Al-Kawthar",
+    englishName: "The Abundance",
+    numberOfAyahs: 3,
+    youtubeId: "BPByXf1GndY" // Example YouTube ID
+  },
+  {
+    id: 109,
+    name: "Al-Kafirun",
+    englishName: "The Disbelievers",
+    numberOfAyahs: 6,
+    youtubeId: "_60ZV5gQRr0" // Example YouTube ID
+  },
+  {
+    id: 110,
+    name: "An-Nasr",
+    englishName: "The Divine Support",
+    numberOfAyahs: 3,
+    youtubeId: "hsXFrrrTws0" // Example YouTube ID
+  },
+  {
+    id: 111,
+    name: "Al-Masad / Al-lahab",
+    englishName: "The Palm Fiber",
+    numberOfAyahs: 5,
+    youtubeId: "BB524Lt1CwQ" // Example YouTube ID
+  },
+  {
+    id: 112,
+    name: "Al-Ikhlas",
+    englishName: "The Sincerity",
+    numberOfAyahs: 4,
+    youtubeId: "LjYa8BGjOqI" // Example YouTube ID
+  },
+  {
+    id: 113,
+    name: "Al-Falaq",
+    englishName: "The Daybreak",
+    numberOfAyahs: 5,
+    youtubeId: "Q2Y82Po1REA" // Example YouTube ID
+  },
+  {
+    id: 114,
+    name: "An-Nas",
+    englishName: "Mankind",
+    numberOfAyahs: 6,
+    youtubeId: "jX2IcXY0ILU" // Example YouTube ID
+  },
 ];
